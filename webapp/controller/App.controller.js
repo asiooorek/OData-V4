449348ee99f8c63723a4418a5670e73c138f965c
62668607 <asia.ryfka@gmail.com>
2020-04-10 14:00:47 +0200

    Step 4: Filtering, Sorting, and Counting

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,9 +1,13 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/ui/model/json/JSONModel",
 	"sap/m/MessageToast",
 	"sap/m/MessageBox",
-], function (Controller, JSONModel, MessageToast, MessageBox) {
+	"sap/ui/model/Sorter",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator",
+	"sap/ui/model/FilterType",
+	"sap/ui/model/json/JSONModel"
+], function (Controller, MessageToast, MessageBox, Sorter, Filter, FilterOperator, FilterType, JSONModel) {
 	"use strict";
 
 	return Controller.extend("sap.ui.core.tutorial.odatav4.controller.App", {
@@ -13,12 +17,22 @@ sap.ui.define([
 		 */
 		onInit : function () {
 			var oJSONData = {
-				busy : false
+				busy : false,
+				order : 0
 			};
 			var oModel = new JSONModel(oJSONData);
 			this.getView().setModel(oModel, "appView");
 		},
 
+
+		/* =========================================================== */
+		/*           begin: event handlers                             */
+		/* =========================================================== */
+
+
+		/**
+		 * Refresh the data.
+		 */
 		onRefresh : function () {
 			var oBinding = this.byId("peopleList").getBinding("items");
 
@@ -30,9 +44,53 @@ sap.ui.define([
 			MessageToast.show(this._getText("refreshSuccessMessage"));
 		},
 
+		/**
+		 * Search for the term in the search field.
+		 */
+		onSearch : function () {
+			var oView = this.getView(),
+				sValue = oView.byId("searchField").getValue(),
+				oFilter = new Filter("LastName", FilterOperator.Contains, sValue);
+
+			oView.byId("peopleList").getBinding("items").filter(oFilter, FilterType.Application);
+		},
+
+		/**
+		 * Sort the table according to the last name.
+		 * Cycles between the three sorting states "none", "ascending" and "descending"
+		 */
+		onSort : function () {
+			var oView = this.getView(),
+				aStates = [undefined, "asc", "desc"],
+				aStateTextIds = ["sortNone", "sortAscending", "sortDescending"],
+				sMessage,
+				iOrder = oView.getModel("appView").getProperty("/order");
+
+			// Cycle between the states
+			iOrder = (iOrder + 1) % aStates.length;
+			var sOrder = aStates[iOrder];
+
+			oView.getModel("appView").setProperty("/order", iOrder);
+			oView.byId("peopleList").getBinding("items").sort(sOrder && new Sorter("LastName", sOrder === "desc"));
+
+			sMessage = this._getText("sortMessage", [this._getText(aStateTextIds[iOrder])]);
+			MessageToast.show(sMessage);
+		},
+
+
+		/* =========================================================== */
+		/*           end: event handlers                               */
+		/* =========================================================== */
+
+
+		/**
+		 * Convenience method for retrieving a translatable text.
+		 * @param {string} sTextId - the ID of the text to be retrieved.
+		 * @param {Array} [aArgs] - optional array of texts for placeholders.
+		 * @returns {string} the text belonging to the given ID.
+		 */
 		_getText : function (sTextId, aArgs) {
 			return this.getOwnerComponent().getModel("i18n").getResourceBundle().getText(sTextId, aArgs);
-
 		}
 	});
 });
